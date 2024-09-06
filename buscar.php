87d3461ce3d0902cfec4e6f3c75c1a8f3575b010
Gorlex <gorlexx@hotmail.com>
2024-09-05 21:47:32 -0500

    tabla nueva

--- buscar.php
+++ buscar.php
@@ -12,8 +12,30 @@
     // Mostrar los resultados
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            echo "<b>SENSOR</b>&nbsp;&nbsp;&nbsp;&nbsp;" . " " . "<b>MEDIDA</b>&nbsp;&nbsp;" . "<b>TIEMPO</b>&nbsp;&nbsp;" . "<br>";
-            echo $row["sensor"] . "&nbsp;&nbsp;" . $row["medida"] . "&nbsp;&nbsp;&nbsp;&nbsp;" . $row["tiempo"] . "<br>";
+         //  echo "<b>SENSOR</b>&nbsp;&nbsp;&nbsp;&nbsp;" . " " . "<b>MEDIDA</b>&nbsp;&nbsp;" . "<b>TIEMPO</b>&nbsp;&nbsp;" . "<br>";
+         //   echo $row["sensor"] . "&nbsp;&nbsp;" . $row["medida"] . "&nbsp;&nbsp;&nbsp;&nbsp;" . $row["tiempo"] . "<br>";
+       
+          echo  "<table>";
+          echo      "<tr>";
+          echo          "<th>SENSOR</th>";
+          echo          "<th>MEDIDA</th>";
+          echo          "<th>TIEMPO</th>";
+          echo      "</tr>";
+
+          echo      "<tr>";
+          echo    "<td>" . $row["sensor"] . "</td>";
+          echo    "<td>" . $row["medida"] . "</td>";
+          echo    "<td>" . $row["tiempo"] . "</td>";
+              
+          echo      "</tr>";
+            
+          echo "</table>";
+       
+       
+       
+       
+       
+       
         }
     } else {
         echo "<h2> No se encontraron resultados </h2>";
